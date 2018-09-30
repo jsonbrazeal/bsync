3cf926345062907d2adbf4453d7b2cddb6aa8d16
Jason Brazeal <jason@jasonbrazeal.com>
2018-09-29 17:12:28 -0700

    working with blank replaceInHomeTree func

--- bsync.js
+++ bsync.js
@@ -1,4 +1,4 @@
-function dataURItoBlob (dataURI) {
+function dataURItoBlob(dataURI) {
     // convert base64 to raw binary data held in a string
     // doesn't handle URLEncoded DataURIs
     var byteString;
@@ -19,53 +19,50 @@ function dataURItoBlob (dataURI) {
 }
 
 function uploadToS3(data) {
-  var base64Text = btoa(data.toString());
+  var base64Text = btoa(data);
   var dataUrl = 'data:text/plain;base64,' + base64Text
   var blobData = dataURItoBlob(dataUrl);
   chrome.storage.local.get({
-    computer: '',
-    target_root: '',
-    target_folder: '',
     s3_bucket: '',
     s3_region: '',
     s3_key_id: '',
     s3_key_secret: '',
     sync_file: '',
   }, function(items) {
-    if (!(items.computer && items.target_root && items.target_folder && items.s3_bucket && items.sync_file, items.s3_region && items.s3_key_secret && items.s3_key_id)) {
+    if (!(items.s3_bucket && items.sync_file, items.s3_region && items.s3_key_secret && items.s3_key_id)) {
       alert('bsync is not correctly configured for uploadToS3 operation');
       chrome.runtime.openOptionsPage();
       return;
     }
     AWS.config.update({accessKeyId: items.s3_key_id, secretAccessKey: items.s3_key_secret});
     AWS.config.region = items.s3_region;
-    console.log('hi')
-    console.log(items)
-    console.log(AWS.config)
     var s3 = new AWS.S3({
       apiVersion: '2006-03-01',
       params: {Bucket: items.s3_bucket}
     });
     var params = {Key: items.sync_file, ContentType: 'plain/text', Body: blobData}
     s3.putObject(params, function (err, data) {
-      console.log(err ? 'error: ' + err.toString() : 'uploaded!');
-      console.log(data);
+      if (err) {
+        console.log('error: ' + err.toString());
+        alert('bsync uploadToS3 error: ' + err.toString());
+      } else {
+        console.log('sync done 😎');
+      }
     });
   });
 }
 
 function downloadFromS3() {
   chrome.storage.local.get({
-    computer: '',
-    target_root: '',
-    target_folder: '',
     s3_bucket: '',
     s3_region: '',
     s3_key_id: '',
     s3_key_secret: '',
     sync_file: '',
+    target_root: '',
+    target_folder: ''
   }, function(items) {
-    if (!(items.computer && items.target_root && items.target_folder && items.s3_bucket && items.sync_file && items.s3_region && items.s3_key_secret && items.s3_key_id)) {
+    if (!(items.s3_bucket && items.sync_file && items.s3_region && items.s3_key_secret && items.s3_key_id && items.target_root && items.target_folder)) {
       alert('bsync is not correctly configured for downloadFromS3 operation');
       chrome.runtime.openOptionsPage();
     }
@@ -80,105 +77,127 @@ function downloadFromS3() {
       Key: items.sync_file
     };
     s3.getObject(params, function(err, data) {
-      console.log(err ? 'error: ' + err.toString() : 'downloaded!');
-      console.log(data);
-      console.log(data.Body.toString());
-      console.log(JSON.parse(data.Body.toString()))
+      if (err) {
+        console.log('error: ' + err.toString());
+        alert('bsync downloadFromS3 error: ' + err.toString());
+      } else {
+        console.log('got bookmarks 🤓');
+        console.log(data.Body.toString());
+        // bookmarks = JSON.parse(decodeURIComponent(escape(window.atob(data.Body.toString()))))
+        bookmarks = JSON.parse(data.Body.toString())
+        replaceInHomeTree(bookmarks, items.target_root, items.target_folder);
+      }
     });
   });
 }
 
 chrome.runtime.onInstalled.addListener(function(details) {
-
-  console.log(details);
+  console.log('bsync: ' + JSON.stringify(details));
   chrome.runtime.openOptionsPage();
-
-});
-
-// Observe bookmark modifications and revert any modifications made to managed
-// bookmarks. The tree is always reloaded in case the events happened while the
-// page was inactive.
-
-chrome.bookmarks.onCreated.addListener(function(id, info) {
-  console.log('bookmark created:');
-  console.log(info);
-  console.log('uploading to s3');
-  // uploadToS3('{ "bookmark": "created" }');
-  downloadFromS3();
-});
-
-chrome.bookmarks.onChildrenReordered.addListener(function(id, info) {
-  console.log('bookmark children reordered');
-  console.log(info);
-});
-
-chrome.bookmarks.onImportEnded.addListener(function(id, info) {
-  console.log('bookmark import ended');
-  console.log(info);
-});
-
-chrome.bookmarks.onMoved.addListener(function(id, info) {
-  // tree.load(function() {
-  //   var managedNode = tree.getById(id);
-  //   if (managedNode && !managedNode.isRoot()) {
-  //     managedNode.moveInModel(info.parentId, info.index, function(){});
-  //   } else {
-  //     // Check if the parent node has managed children that need to move.
-  //     // Example: moving a non-managed bookmark in front of the managed
-  //     // bookmarks.
-  //     var parentNode = tree.getById(info.parentId);
-  //     if (parentNode)
-  //       parentNode.reorderChildren();
-  //   }
-  // });
-  console.log('bookmark moved');
-  console.log(info);
-});
-
-chrome.bookmarks.onChanged.addListener(function(id, info) {
-  // tree.load(function() {
-  //   var managedNode = tree.getById(id);
-  //   if (!managedNode || managedNode.isRoot())
-  //     return;
-  //   chrome.bookmarks.update(id, {
-  //     'title': managedNode._title,
-  //     'url': managedNode._url
-  //   });
-  // });
-  console.log('bookmark changed');
-  console.log(info);
-});
-
-chrome.bookmarks.onRemoved.addListener(function(id, info) {
-  // tree.load(function() {
-  //   var managedNode = tree.getById(id);
-  //   if (!managedNode || managedNode.isRoot())
-  //     return;
-  //   // A new tree.store() is needed at the end because the regenerated nodes
-  //   // will have new IDs.
-  //   var callbackChain = new CallbackChain();
-  //   callbackChain.push(tree.store.bind(tree));
-  //   managedNode.regenerate(info.parentId, info.index, callbackChain);
-  // });
-  console.log('bookmark removed');
-  console.log(info);
-});
-
-// // Important note: if you make an extension with an Event page ("persistent": false in the manifest), setInterval with 5-minute interval will fail as the background page will get unloaded.
-
-// // If your extension uses window.setTimeout() or window.setInterval(), switch to using the alarms API instead. DOM-based timers won't be honored if the event page shuts down.
-
-// // In this case, you need to implement it using chrome.alarms API:
-
-chrome.alarms.create('5min', {
-  delayInMinutes: 5,
-  periodInMinutes: 5
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  if (alarm.name === '5min') {
-    console.log('5min alarm!!! ' + new Date().toUTCString())
+  if (alarm.name === 'bsync') {
+    console.log('💻 bsync starting ' + new Date().toUTCString())
+    performSync();
   }
 });
 
-// // In case of persistent background pages, setInterval is still an acceptable solution.
+function performSync() {
+  chrome.storage.local.get({
+    computer: ''
+  }, function(items) {
+    if (items.computer == 'remote') {
+      var bookmarkTreeNodes = chrome.bookmarks.getTree(
+        function(bookmarkTreeNodes) {
+          console.log(bookmarkTreeNodes);
+          var bookmarkTreeNodesString= unescape(encodeURIComponent(JSON.stringify(bookmarkTreeNodes)));
+          uploadToS3(bookmarkTreeNodesString);
+        }
+      );
+    } else if (items.computer == 'home') {
+      downloadFromS3();
+    } else {
+      console.log('unknown computer settings ' + items.computer);
+    }
+  });
+}
+
+function replaceInHomeTree(bookmarks, target_root, target_folder) {
+  var bookmarkTreeNodes = chrome.bookmarks.getTree(
+    function(bookmarkTreeNodes) {
+      console.log(bookmarkTreeNodes);
+
+    }
+  );
+
+}
+
+// // Observe bookmark modifications and revert any modifications made to managed
+// // bookmarks. The tree is always reloaded in case the events happened while the
+// // page was inactive.
+
+// chrome.bookmarks.onCreated.addListener(function(id, info) {
+//   console.log('bookmark created:');
+//   console.log(info);
+//   console.log('uploading to s3');
+//   // uploadToS3('{ "bookmark": "created" }');
+//   // downloadFromS3();
+// });
+
+// chrome.bookmarks.onChildrenReordered.addListener(function(id, info) {
+//   console.log('bookmark children reordered');
+//   console.log(info);
+// });
+
+// chrome.bookmarks.onImportEnded.addListener(function(id, info) {
+//   console.log('bookmark import ended');
+//   console.log(info);
+// });
+
+// chrome.bookmarks.onMoved.addListener(function(id, info) {
+//   // tree.load(function() {
+//   //   var managedNode = tree.getById(id);
+//   //   if (managedNode && !managedNode.isRoot()) {
+//   //     managedNode.moveInModel(info.parentId, info.index, function(){});
+//   //   } else {
+//   //     // Check if the parent node has managed children that need to move.
+//   //     // Example: moving a non-managed bookmark in front of the managed
+//   //     // bookmarks.
+//   //     var parentNode = tree.getById(info.parentId);
+//   //     if (parentNode)
+//   //       parentNode.reorderChildren();
+//   //   }
+//   // });
+//   console.log('bookmark moved');
+//   console.log(info);
+// });
+
+// chrome.bookmarks.onChanged.addListener(function(id, info) {
+//   // tree.load(function() {
+//   //   var managedNode = tree.getById(id);
+//   //   if (!managedNode || managedNode.isRoot())
+//   //     return;
+//   //   chrome.bookmarks.update(id, {
+//   //     'title': managedNode._title,
+//   //     'url': managedNode._url
+//   //   });
+//   // });
+//   console.log('bookmark changed');
+//   console.log(info);
+// });
+
+// chrome.bookmarks.onRemoved.addListener(function(id, info) {
+//   // tree.load(function() {
+//   //   var managedNode = tree.getById(id);
+//   //   if (!managedNode || managedNode.isRoot())
+//   //     return;
+//   //   // A new tree.store() is needed at the end because the regenerated nodes
+//   //   // will have new IDs.
+//   //   var callbackChain = new CallbackChain();
+//   //   callbackChain.push(tree.store.bind(tree));
+//   //   managedNode.regenerate(info.parentId, info.index, callbackChain);
+//   // });
+//   console.log('bookmark removed');
+//   console.log(info);
+// });
--- options.js
+++ options.js
@@ -8,6 +8,7 @@ function save_settings() {
   var s3_key_secret = document.getElementById('s3_key_secret').value;
   var s3_bucket = document.getElementById('s3_bucket').value;
   var s3_region = document.getElementById('s3_region').value;
+  var last_modified = new Date().toUTCString();
 
   chrome.storage.local.set({
     computer: computer,
@@ -17,22 +18,30 @@ function save_settings() {
     s3_key_secret: s3_key_secret,
     s3_bucket: s3_bucket,
     s3_region: s3_region,
-    sync_file: 'jason.bsync'
+    sync_file: 'jason.bsync',
+    last_modified: last_modified
   }, function() {
-    document.getElementById('status').textContent = 'settings saved ' + new Date().toUTCString();
+    document.getElementById('status').textContent = 'settings saved ' + last_modified;
+    chrome.extension.getBackgroundPage().console.log(computer + ' computer settings saved ' + last_modified);
+    chrome.extension.getBackgroundPage().console.log('\u{23F1} alarm created...sync will occur every 5 minutes starting in 1 minute');
+    chrome.alarms.create('bsync', {
+      delayInMinutes: 1,
+      periodInMinutes: 5
+    });
   });
 }
 
 // Restores select box and checkbox state using the preferences stored in chrome.storage
 function restore_settings() {
   chrome.storage.local.get({
-      computer: 'home',
-      target_root: 'bar',
+      computer: '',
+      target_root: '',
       target_folder: '',
       s3_key_id: '',
       s3_key_secret: '',
       s3_bucket: '',
       s3_region: '',
+      last_modified: ''
     }, function(items) {
       document.getElementById('computer').value = items.computer;
       document.getElementById('target_root').value = items.target_root;
@@ -41,6 +50,7 @@ function restore_settings() {
       document.getElementById('s3_key_secret').value = items.s3_key_secret;
       document.getElementById('s3_bucket').value = items.s3_bucket;
       document.getElementById('s3_region').value = items.s3_region;
+      document.getElementById('status').textContent = 'settings saved ' + items.last_modified;
     });
 }
 
